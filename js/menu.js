05fcc10b4d57c909db1b474bb3768c60e0a95f71
macaron924 <121280397+macaron924@users.noreply.github.com>
2022-12-23 23:19:26 +0900

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
