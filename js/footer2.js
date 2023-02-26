fefff7e6a6dc03322cbc2084c86477727efed8a8
マカロン <121280397+macaron924@users.noreply.github.com>
2023-02-26 17:16:19 +0900

    Add files via upload

---
+++ js/footer2.js
@@ -0,0 +1,6 @@
+document.write(`
+<footer>
+    <h3>はっぴーゆにこーん☆</h3>
+    <div class="quote-box">このサイトで使用されている著作物(画像・データ等)は全て<br>©︎AZONE INTERNATIONAL・acus/アサルトリリィプロジェクト が保有しております。</div>
+</footer>
+`);
