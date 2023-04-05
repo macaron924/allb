8e402ff0808765b04745c2526941d5bdd509f509
マカロン <121280397+macaron924@users.noreply.github.com>
2023-04-05 23:43:12 +0900

    Add files via upload

---
+++ gacha_info/GetGachaTicketList.js
@@ -0,0 +1,19 @@
+for (var i = 0; i < gachaTicketJson.length; i++) {
+    content = `
+    <div class="flex_ticket-item">
+        <table>
+            <tr><td rowspan="2"><a href="./ticket/index.html?index=${gachaTicketJson[i]['index']}"><img src="../images/item/gacha-ticket_${gachaTicketJson[i]['index']}.png"></a></td>
+                <td><a href="./ticket/index.html?index=${gachaTicketJson[i]['index']}">${gachaTicketJson[i]['name']}</a></td></tr>
+            <tr><td>※${gachaTicketJson[i]['lineup']['else'][0]['caption']}</td></tr>
+        </table>
+    </div>
+    `;
+    document.getElementById('gacha-ticlet-list').insertAdjacentHTML('beforeend', content);
+}
+
+const images = document.querySelectorAll('img');
+images.forEach((image) => {
+    image.addEventListener('error',() => {
+        image.setAttribute('src', '../images/no-image.png');
+    });
+});
