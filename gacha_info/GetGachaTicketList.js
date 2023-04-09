cba19f16a36d85cb22a3c2be573be74dab917c32
マカロン <121280397+macaron924@users.noreply.github.com>
2023-04-09 15:58:33 +0900

    Update GetGachaTicketList.js

--- gacha_info/GetGachaTicketList.js
+++ gacha_info/GetGachaTicketList.js
@@ -2,7 +2,7 @@ for (var i = 0; i < gachaTicketJson.length; i++) {
     content = `
     <div class="flex_ticket-item">
         <table>
-            <tr><td rowspan="2"><a href="./ticket/index.html?index=${gachaTicketJson[i]['index']}"><img src="../images/item/gacha-ticket_${gachaTicketJson[i]['index']}.png"></a></td>
+            <tr><td rowspan="2"><a href="./ticket/?index=${gachaTicketJson[i]['index']}"><img src="../images/item/gacha-ticket_${gachaTicketJson[i]['index']}.png"></a></td>
                 <td><a href="./ticket/index.html?index=${gachaTicketJson[i]['index']}">${gachaTicketJson[i]['name']}</a></td></tr>
             <tr><td>※${gachaTicketJson[i]['lineup']['else'][0]['caption']}</td></tr>
         </table>
