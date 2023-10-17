b58393b3f617679e515e13695586144293e6a676
マカロン <akarin915mor@gmail.com>
2023-10-17 19:38:21 +0900

    レジェンダリーメダル計算機能アップデート

--- legendary_calc/script.js
+++ legendary_calc/script.js
@@ -8,36 +8,158 @@ $("#reward-display").click(function () {
     $("#reward_table").toggleClass('active');
 });
 
+$("input").change(function () {
+    inputChange();
+});
+
+$("select").change(function () {
+    inputChange();
+});
+
 function inputChange() {
     document.getElementById("calc-btn").className = "before-change";
     document.getElementById("calc-btn").value = "未反映";
 }
 
+function printError() {
+    document.getElementById("calc-btn").className = "error";
+    document.getElementById("calc-btn").value = "エラー";
+}
+
+function reset() {
+    document.getElementById('diffDays').innerText = "-";
+    document.getElementById('numOfMonday').innerText = "-";
+    document.getElementById("daily_times_1").innerText = "-";
+    document.getElementById("daily_times_2").innerText = "-";
+    document.getElementById("weekly_times_1").innerText = "-";
+    document.getElementById("weekly_times_2").innerText = "-";
+}
+
 function calc() {
+
+    reset();
+
+    let input_true_flag = true; // 1つでも入力が不正な値の場合false
+    let startDate_true_flag = true; // 開始日が不正の場合false
+    let finishDate_true_flag = true; // 終了日が不正の場合false
 
     // 往復モード
     const oufuku = parseInt(document.getElementById('oufuku').value);
 
-    // 数値に変換
+    // 日付を取得し数値に変換
     let y1 = parseInt(document.getElementById('year1').value);
     let m1 = parseInt(document.getElementById('month1').value);
     let d1 = parseInt(document.getElementById('date1').value);
     let y2 = parseInt(document.getElementById('year2').value);
     let m2 = parseInt(document.getElementById('month2').value);
     let d2 = parseInt(document.getElementById('date2').value);
+    document.getElementById('year1').classList.remove("error");
+    document.getElementById('month1').classList.remove("error");
+    document.getElementById('date1').classList.remove("error");
+    document.getElementById('year2').classList.remove("error");
+    document.getElementById('month2').classList.remove("error");
+    document.getElementById('date2').classList.remove("error");
+
+    // 所持取得
+    let have_reward = {};
+    for (let i = 0; i < reward_index.length; i++) {
+        document.getElementById(`input_have_${reward_index[i]}`).classList.remove("error");
+        have_reward[reward_index[i]] = parseInt(document.getElementById(`input_have_${reward_index[i]}`).value);
+        if (have_reward[reward_index[i]] < 0) {
+            document.getElementById(`input_have_${reward_index[i]}`).classList.add("error");
+            input_true_flag = false;
+        }
+    }
+
+    // 目標の取得
+    let goal_item = {};
+    for (let j = 0; j < item_index.length; j++) {
+        document.getElementById(`input_goal_${item_index[j]}`).classList.remove("error");
+        goal_item[item_index[j]] = parseInt(document.getElementById(`input_goal_${item_index[j]}`).value);
+        if (goal_item[item_index[j]] < 0) {
+            document.getElementById(`input_goal_${item_index[j]}`).classList.add("error");
+            input_true_flag = false;
+        }
+    }
+
+    // 日付の整合性チェック
+    const max_date = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (m1 < 1 || 12 < m1) {
+        document.getElementById('month1').classList.add("error");
+        startDate_true_flag = false;
+        if (d1 < 1 || 31 < d1) {
+            document.getElementById('date1').classList.add("error");
+            startDate_true_flag = false;
+        }
+    } else {
+        let max_date_copy = max_date.concat();
+        if (y1 % 4 == 0) max_date_copy[1] = 29;
+        if (d1 < 1 || max_date_copy[m1 - 1] < d1) {
+            document.getElementById('date1').classList.add("error");
+            startDate_true_flag = false;
+        }
+    }
+    if (m2 < 1 || 12 < m2) {
+        document.getElementById('month2').classList.add("error");
+        finishDate_true_flag = false;
+        if (d2 < 1 || 31 < d2) {
+            document.getElementById('date2').classList.add("error");
+            finishDate_true_flag = false;
+        }
+    } else {
+        let max_date_copy = max_date.concat();
+        if (y2 % 4 == 0) max_date_copy[1] = 29;
+        if (d2 < 1 || max_date_copy[m2 - 1] < d2) {
+            document.getElementById('date2').classList.add("error");
+            finishDate_true_flag = false;
+        }
+    }
+    
+    // 開始日が終了日より過去になっていないか
+    if (y1 > y2) {
+        document.getElementById('year1').classList.add("error");
+        document.getElementById('year2').classList.add("error");
+        input_true_flag = false;
+    } else if (y1 == y2 && m1 > m2) {
+        document.getElementById('month1').classList.add("error");
+        document.getElementById('month2').classList.add("error");
+        input_true_flag = false;
+    } else if (m1 == m2 && d1 > d2) {
+        document.getElementById('date1').classList.add("error");
+        document.getElementById('date2').classList.add("error");
+        input_true_flag = false;
+    }
 
     // 日付オブジェクト生成
     let startDate = new Date(y1, m1-1, d1);
     let finishDate = new Date(y2, m2-1, d2);
-
-    // 曜日表示
-    let weekdayName = "日月火水木金土";
     let startDay = startDate.getDay();
     let finishDay = finishDate.getDay()
-    document.getElementById('day1').innerText = weekdayName[startDay];
-    document.getElementById('day2').innerText = weekdayName[finishDay];
+
+    // 曜日表示 入力が正しい場合のみ行う
+    let weekdayName = "日月火水木金土";
+    if (startDate_true_flag == true) {
+        document.getElementById('day1').innerText = weekdayName[startDay];
+    } else {
+        document.getElementById('day1').innerText = "-";
+        input_true_flag = false;
+    }
+    if (finishDate_true_flag == true) {
+        document.getElementById('day2').innerText = weekdayName[finishDay];
+    } else {
+        document.getElementById('day2').innerText = "-";
+        input_true_flag = false;
+    }
+
+    // 入力に不正な値があった場合ここで中断
+    if (input_true_flag != true) {
+        printError();
+        return false;
+    }
 
     // 期間計算(開始日・終了日ともに含む)
+    let already_get = document.getElementById("already_get").checked;
+    if (already_get == true) startDate.setDate(startDate.getDate() + 1);
     let diffMilliSec = finishDate - startDate;
     let diffDays = parseInt(diffMilliSec / 1000 / 60 / 60 / 24) + 1;
     document.getElementById('diffDays').innerText = diffDays;
@@ -65,7 +187,7 @@ function calc() {
     }
     //console.log(dayArray);
 
-    // 週ごとの日数を往復で分ける
+    // 週ごとの日数・月曜の回数を往復で分ける
     let kaisu = { daily: [[], []], dailyTotal: [0, 0], weekly: [[], []], weeklyTotal: [0, 0] };
     if (oufuku == 1) {
         for (let i = 0; i < dayArray.length; i++) {
@@ -110,7 +232,6 @@ function calc() {
 
     // 総獲得報酬計算
     let get_reward = {};
-    let have_reward = {};
     let after_reward = {};
     for (let i = 0; i < reward_index.length; i++) {
         get_reward[reward_index[i]] = 0;
@@ -118,17 +239,14 @@ function calc() {
         get_reward[reward_index[i]] += daily_reward[grade[1]][reward_index[i]] * kaisu['dailyTotal'][1];
         get_reward[reward_index[i]] += weekly_reward[grade[0]][ranking[0]][reward_index[i]] * kaisu['weeklyTotal'][0];
         get_reward[reward_index[i]] += weekly_reward[grade[1]][ranking[1]][reward_index[i]] * kaisu['weeklyTotal'][1];
-        document.getElementById(`get_${reward_index[i]}`).innerText = get_reward[reward_index[i]];
-        have_reward[reward_index[i]] = parseInt(document.getElementById(`input_have_${reward_index[i]}`).value);
         after_reward[reward_index[i]] = get_reward[reward_index[i]] + have_reward[reward_index[i]];
+        document.getElementById(`get_${reward_index[i]}`).innerText = get_reward[reward_index[i]];
         document.getElementById(`have_${reward_index[i]}`).innerText = have_reward[reward_index[i]];
         document.getElementById(`after_${reward_index[i]}`).innerText = after_reward[reward_index[i]];
     }
 
-    // 目標の取得
-    let goal_item = {};
+    // 目標の書き込み
     for (let j = 0; j < item_index.length; j++) {
-        goal_item[item_index[j]] = parseInt(document.getElementById(`input_goal_${item_index[j]}`).value);
         document.getElementById(`goal_${item_index[j]}`).innerText = goal_item[item_index[j]];
     }
 
@@ -173,12 +291,12 @@ function calc() {
     document.getElementById('exchange_need').innerText = exchange_need;
     
     let exchange_lack = goal_reward['medal'] + exchange_need - after_reward['medal'];
-    if (exchange_lack < 0) {
-        document.getElementById('exchange_lack').innerText = exchange_lack * -1 + " 余り";
-        document.getElementById('exchange_lack').className = "plus";
-    } else {
+    if (exchange_lack > 0) {
         document.getElementById('exchange_lack').innerText = exchange_lack + " 不足";
         document.getElementById('exchange_lack').className = "minus";
+    } else {
+        document.getElementById('exchange_lack').innerText = exchange_lack * -1 + " 余り";
+        document.getElementById('exchange_lack').className = "plus";
     }
     
     document.getElementById("calc-btn").className = "";
