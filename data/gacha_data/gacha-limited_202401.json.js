81a391d782e4732248358d2bf46d8a0c835991af
macaron924 <akarin915mor@gmail.com>
2024-01-01 19:01:08 +0900

    ガチャの追加
    三賀日限定!!スペシャルムーンフェス
    マンデーボーナスガチャ
    5回限定!!あけましておめでとうガチャ-2024-

---
+++ data/gacha_data/gacha-limited_202401.json.js
@@ -0,0 +1,112 @@
+const gachaLimited202401 = [
+    {
+        start: "2024/1/1(月) 12:00",
+        finish: "2024/1/8(月) 23:59",
+        type: "その他のガチャ",
+        medal: "medal_bonus_202401",
+        gacha: [
+            {
+                index: "20240101_2",
+                name: "5回限定!!あけましておめでとうガチャ-2024-",
+                big_banner: true,
+                bonus: {
+                    notice: [
+                        { text: "11回ガチャ5回限定", item: [] },
+                        { text: "ガチャを引く毎に", item: [ { index: "medal", value: 300}, { index: "goggle-3", value: 5 } ] }
+                    ],
+                    table_header: ["11回ガチャ回数", "累計", "ガチャボーナス"],
+                    table_content: [
+                        { need: [1, 11], item: [ { index: "gacha-ticket_new-year-cost20costume", value: 1 } ] },
+                        { need: [2, 22], item: [ { index: "gacha-ticket_new-year-cost20costume", value: 1 } ] },
+                        { need: [3, 33], item: [ { index: "gacha-ticket_new-year-cost21", value: 1 } ] },
+                        { need: [4, 44], item: [ { index: "gacha-ticket_new-year-cost21", value: 1 } ] },
+                        { need: [5, 55], item: [ { index: "gacha-ticket_new-year-cost21costume", value: 1 } ] }
+                    ]
+                },
+                lineup: {
+                    total_ratio: [5],
+                    notice: ["★5メモリアはコスト21以上のみ"],
+                    pu: [],
+                    else: { caption: "ピックアップ無し", content: [359, 360, 367, 368, 369, 370, 373, 383, 379, 380, 381, 382, 386, 387, 388, 389, 397, 398, 399, 400, 401, 403, 406, 407, 408, 409, 426, 480, 481, 482, 483, 484, 490, 494, 498, 499, 500, 501, 502, 508, 509, 526, 533, 534, 535, 536, 548, 549, 550, 551, 552, 553, 557, 558, 559, 560, 561, 575, 566, 567, 568, 569, 570, 582, 596, 606, 607, 608, 609, 610, 611, 612, 614, 615, 624, 625, 626, 627, 633, 634, 635, 636, 638, 639, 644, 645, 646, 647, 648, 641, 642, 643, 649, 650, 651, 652, 653, 654, 655, 657, 658, 659, 660, 666, 667, 668, 669, 662, 663, 664, 665, 675, 676, 677, 680, 681, 682, 683, 684, 685, 686, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 709, 710, 711, 712, 713, 715, 716, 717, 718, 719, 720, 721, 722] }
+                }
+            }
+        ]
+    },
+    {
+        start: "2024/1/1(月) 12:00",
+        finish: "2024/1/3(水) 23:59",
+        type: "マンデーボーナスガチャ",
+        medal: "medal_bonus_202401",
+        gacha: [
+            {
+                index: "20240101_3",
+                name: "マンデーボーナスガチャ",
+                big_banner: true,
+                bonus: {
+                    notice: [
+                        { text: "11回ガチャ1回限定", item: [] },
+                        { text: "ガチャを引くと", item: [ { index: "medal", value: 200}, { index: "skip-ticket", value: 300 }, { index: "goggle-3", value: 5 } ] }
+                    ],
+                    table_header: [],
+                    table_content: []
+                },
+                lineup: {
+                    total_ratio: [5],
+                    notice: ["★5メモリアは半年以内追加のみ"],
+                    pu: [],
+                    else: { caption: "ピックアップ無し", content: [684, 686, 694, 695, 696, 702, 703, 704] }
+                }
+            }
+        ]
+    },
+    {
+        start: "2024/1/1(月) 0:00",
+        finish: "2024/1/3(水) 23:59",
+        type: "その他のガチャ",
+        medal: "medal_bonus_202401",
+        gacha: [
+            {
+                index: "20240101_1",
+                name: "三賀日限定!!スペシャルムーンフェス",
+                big_banner: true,
+                bonus: {
+                    notice: [
+                        { text: "11回ガチャ20回限定", item: [] },
+                        { text: "ガチャを引く毎に", item: [ { index: "medal", value: 100}, { index: "goggle-3", value: 5 } ] }
+                    ],
+                    table_header: ["11回ガチャ回数", "累計", "ガチャボーナス"],
+                    table_content: [
+                        { need: [1, 11], item: [ { index: "medal_pendant-premium", value: 300 } ] },
+                        { need: [2, 22], item: [ { index: "moon", value: 1 } ] },
+                        { need: [3, 33], item: [ { index: "exchange_memoria_new-year-2024-normal", value: 1 } ] },
+                        { need: [4, 44], item: [ { index: "medal_pendant-royal", value: 100 } ] },
+                        { need: [5, 55], item: [ { index: "moon", value: 1 } ] },
+                        { need: [6, 66], item: [ { index: "medal_pendant-premium", value: 50 } ] },
+                        { need: [7, 77], item: [ { index: "skill-all-3", value: 50 } ] },
+                        { need: [8, 88], item: [ { index: "moon", value: 1 } ] },
+                        { need: [9, 99], item: [ { index: "exchange_memoria_new-year-2024-premium", value: 1 } ] },
+                        { need: [10, 110], item: [ { index: "medal_pendant-premium", value: 50 } ] },
+                        { need: [11, 121], item: [ { index: "skill-all-3", value: 50 } ] },
+                        { need: [12, 132], item: [ { index: "moon", value: 1 } ] },
+                        { need: [13, 143], item: [ { index: "medal_pendant-royal", value: 100 } ] },
+                        { need: [14, 154], item: [ { index: "medal_pendant-premium", value: 50 } ] },
+                        { need: [15, 165], item: [ { index: "exchange_memoria_new-year-2024-premium", value: 1 } ] },
+                        { need: [16, 176], item: [ { index: "moon", value: 1 } ] },
+                        { need: [17, 187], item: [ { index: "medal_pendant-premium", value: 50 } ] },
+                        { need: [18, 198], item: [ { index: "skill-all-3", value: 50 } ] },
+                        { need: [19, 209], item: [ { index: "medal_pendant-premium", value: 50 } ] },
+                        { need: [20, 220], item: [ { index: "moon", value: 1 } ] }
+                    ]
+                },
+                lineup: {
+                    total_ratio: [5],
+                    notice: ["★5メモリアはコスト18以上のみ"],
+                    pu: [],
+                    else: { caption: "ピックアップ無し", content: [123, 148, 149, 153, 155, 158, 159, 161, 162, 169, 173, 170, 171, 172, 174, 175, 176, 186, 191, 192, 187, 188, 189, 193, 190, 198, 202, 203, 204, 206, 208, 209, 210, 211, 213, 214, 215, 216, 218, 220, 221, 232, 233, 250, 251, 261, 262, 263, 264, 277, 278, 279, 286, 288, 289, 287, 298, 299, 300, 302, 303, 304, 305, 307, 308, 318, 320, 322, 323, 324, 326, 327, 319, 321, 329, 330, 331, 332, 338, 334, 335, 336, 337, 345, 346, 349, 350, 351, 352, 353, 354, 355, 356, 364, 365, 367, 368, 369, 370, 371, 372, 373, 379, 380, 381, 382, 386, 387, 388, 389, 391, 392, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 410, 411, 406, 407, 408, 409, 414, 415, 416, 418, 419, 420, 421, 431, 432, 427, 428, 429, 430, 444, 445, 446, 447, 448, 449, 451, 452, 453, 454, 455, 456, 457, 458, 461, 467, 468, 469, 470, 471, 474, 475, 480, 481, 482, 483, 484, 498, 499, 500, 501, 502, 504, 505, 538, 539, 533, 534, 535, 536, 548, 549, 550, 551, 552, 564, 557, 558, 559, 560, 561, 566, 567, 568, 569, 570, 578, 579, 580, 581, 586, 587, 588, 589, 591, 592, 602, 603, 604, 605, 609, 610, 611, 612, 614, 615, 624, 625, 626, 627, 633, 634, 635, 636, 638, 639, 650, 652, 663, 665] }
+                }
+            }
+        ]
+    }
+]
+
+gachaLimitedList.push( { ref: gachaLimited202401, index: 202401, active: true, group: "" } );
