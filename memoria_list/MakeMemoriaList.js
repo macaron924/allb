f5789102298462c486027de1cced04809f3a777f
macaron924 <akarin915mor@gmail.com>
2024-01-29 09:09:46 +0900

    メモリア一覧の機能追加
    付属衣装を表示できる機能の追加

--- memoria_list/MakeMemoriaList.js
+++ memoria_list/MakeMemoriaList.js
@@ -1,3 +1,29 @@
+$(document).on('click', '.switch-icon', function() { // 絞り込み選択表示ボタンクリック時
+    $(this).parent().children(".costume").toggleClass('active');
+    $(this).parent().children(".memoria").toggleClass('active');
+});
+
+function getCharaName(charaID) {
+    for (let i = 0; i < charaJson.length; i++) {
+        if (charaJson[i]['charaID'] == charaID) return charaJson[i]['charaName'];
+    }
+    return "";
+}
+
+function getCostumeChara(id) {
+    for (let i = 0; i < costumeJson.length; i++) {
+        if (costumeJson[i]['id'] == id) return getCharaName(costumeJson[i]['chara']);
+    }
+    return "";
+}
+
+function getCostumeName(id) {
+    for (let i = 0; i < costumeJson.length; i++) {
+        if (costumeJson[i]['id'] == id) return costumeJson[i]['name'];
+    }
+    return "";
+}
+
 function addMemoriaList() {
     // flexboxを生成
     let flexbox = document.createElement('div');
@@ -9,7 +35,18 @@ function addMemoriaList() {
         // flexitemを生成
         let flexitem = document.createElement('div');
         flexitem.className = "flex_test-item";
-        flexitem.innerHTML = `<img src="../images/memoria/memoria_${memoriaJson[i]['id']}.jpg" loading="lazy"><br>${memoriaJson[i]['name']}`;
+
+        let innerHtml = `<div class="memoria active"><img src="../images/memoria/memoria_${memoriaJson[i]['id']}.jpg" loading="lazy"><br>${memoriaJson[i]['name']}</div>`;
+
+        let costumeId = memoriaJson[i]['costume'];
+        if (costumeId > 0) {
+            let costumeChara = getCostumeChara(costumeId);
+            let costumeName = getCostumeName(costumeId);
+            innerHtml += `<div class="costume"><img src="../images/costume/costume_${costumeId}.jpg" loading="lazy"><br>${costumeChara} / ${costumeName}</div>`;
+            innerHtml = `<div class="switch-icon"></div>` + innerHtml;
+        }
+
+        flexitem.innerHTML = innerHtml;
 
         // flexitemをflexboxの子要素に追加
         flexbox.appendChild(flexitem);
