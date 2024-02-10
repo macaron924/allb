eac77b68404c9dc0feb6ce90c93872cfe079e5b0
マカロン <121280397+macaron924@users.noreply.github.com>
2024-02-10 10:37:13 +0900

    2/9更新情報 (#297)
    * 新メモリアと新スキルの追加
    
    クリエイターズコラボ-せんぱいのお花たち-
    クリエイターズコラボ-可憐な香に包まれて-
    
    * 新メモリア(低レアリティ)の追加
    
    美岳誕生日2024
    
    * メモリア入手難易度の記述と分類変更
    
    * 新衣装の追加と既存衣装の情報修正
    
    灯莉/ビューティフルワールドα
    
    * 新ガチャの追加
    
    BRAND-NEWメモリアガチャ

--- data/low-rare-memoria.json.js
+++ data/low-rare-memoria.json.js
@@ -383,5 +383,6 @@ const lowRareMemoriaJson = [
     { "id": 382, "name": "楓誕生日2024", "chara": [3] },
     { "id": 383, "name": "3rd Anniversary -PRECIOUS DREAM-", "chara": [1, 2, 35] },
     { "id": 384, "name": "3rd Anniversary -MYSTIC DREAM-", "chara": [998, 72, 103] },
-    { "id": 385, "name": "神琳誕生日2024", "chara": [7] }
+    { "id": 385, "name": "神琳誕生日2024", "chara": [7] },
+    { "id": 386, "name": "美岳誕生日2024", "chara": [72] }
 ]
