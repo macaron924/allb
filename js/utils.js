e14f35d287ff5cae7bc360baffce3f0ad4f0f753
マカロン <121280397+macaron924@users.noreply.github.com>
2024-06-04 09:54:20 +0900

    6/2,3更新情報 (#380)
    * レアスキル違いキャラの追加、画像差し替え
    
    森辰姫（Eルナティックトランサー）
    田中壱（エルヴンマギプロヴィデンス）
    
    * 新メモリアと新スキルの追加
    
    きこえているよ
    プロムナード・プランセス
    
    * 新衣装の追加
    
    森辰姫/黄昏の聖装
    田中壱/黄昏の聖装
    岡田綺更/百合ヶ丘標準制服
    
    * 新CHARMの追加
    
    デュランダルSP.TS (紫恵楽)
    
    * ガチャの追加
    
    ★5メモリア確定！デイリー3回オトクガチャ
    白百合の十二聖剣 記念 辰姫＆壱ガチャ
    マンデーボーナスガチャ

--- js/utils.js
+++ js/utils.js
@@ -12,9 +12,10 @@ function getLimitedGachaData ( depth ) {
     }
     const gachaLimitedList = [];
     
+    gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202406.json"), index: 202406, active: true, group: "" } );
     gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202405.json"), index: 202405, active: true, group: "" } );
-    gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202404.json"), index: 202404, active: true, group: "" } );
-    gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202403.json"), index: 202403, active: true, group: "" } );
+    gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202404.json"), index: 202404, active: false, group: "" } );
+    gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202403.json"), index: 202403, active: false, group: "" } );
     gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202402.json"), index: 202402, active: false, group: "" } );
     gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202401.json"), index: 202401, active: false, group: "" } );
     gachaLimitedList.push( { ref: loadData(path + "data/gacha_data/gacha-limited_202312.json"), index: 202312, active: false, group: "" } );
