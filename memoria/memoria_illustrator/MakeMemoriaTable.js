6e431d3b88ff8b0ed96a000fd2de3a4984794eb2
macaron924 <akarin915mor@gmail.com>
2024-07-14 19:21:38 +0900

    クリエイターズコラボメモリアのイラストレーター情報追加

---
+++ memoria/memoria_illustrator/MakeMemoriaTable.js
@@ -0,0 +1,100 @@
+// テーブル作成
+function makeTable() {
+
+    // table要素を生成
+    let table = document.createElement("table");
+
+    // ヘッダーを作成
+    let tr = document.createElement("tr");
+    // td要素を生成
+    let thId = document.createElement("th");
+    let thName = document.createElement("th");
+    let thIllustrator = document.createElement("th");
+    // th要素内にテキストを追加
+    thId.textContent = "画像";
+    thName.textContent = "メモリア名";
+    thIllustrator.textContent = "イラストレーター";
+    // th要素をtr要素の子要素に追加
+    tr.appendChild(thId);
+    tr.appendChild(thName);
+    tr.appendChild(thIllustrator);
+    // tr要素をtable要素の子要素に追加
+    table.appendChild(tr);
+
+    // 検索結果件数を保存する変数
+    let resultCount = 0;
+
+    // テーブル本体を作成
+    for (let id in memoriaJsonCopy) {
+        if (memoriaJsonCopy[id]["illustration"]["type"] != "クリエイターズコラボ") continue;
+
+        // 検索結果件数カウント
+        resultCount++;
+
+        // tr要素を生成
+        let tr = document.createElement("tr");
+        // td要素を生成
+        let tdId = document.createElement("td");
+        let tdName = document.createElement("td");
+        let tdIllustrator = document.createElement("td");
+        // サムネ画像要素の追加
+        let img = document.createElement("img");
+        if (mode == "lowrare") {
+            img.src = "../../images/low-rare-memoria/low-rare-memoria_" + id + ".webp";
+        } else {
+            img.src = "../../images/memoria/memoria_" + id + ".webp";
+        }
+        img.className = "memoria_img";
+        img.loading = "lazy";
+        // td要素内にテキストを追加
+        tdId.appendChild(img);
+        tdName.textContent = memoriaJsonCopy[id]["name"];
+        tdIllustrator.textContent = memoriaJsonCopy[id]["illustration"]["illustrator"];
+        // リンク
+        if (mode != "lowrare") {
+            let p = document.createElement("p");
+            let link = document.createElement("a");
+            link.href = `../../memoria/detail/?memoriaID=${id}`;
+            link.innerText = "詳細＞＞";
+            p.appendChild(link);
+            tdId.appendChild(p);
+        }
+        // td要素をtr要素の子要素に追加
+        tr.appendChild(tdId);
+        tr.appendChild(tdName);
+        tr.appendChild(tdIllustrator);
+        // tr要素をtable要素の子要素に追加
+        table.appendChild(tr);
+
+        memoriaJsonCopy[id]["tr"] = tr;
+    }
+    // 生成したtable要素を追加する
+    document.getElementById("maintable").replaceChildren(table);
+
+    // 検索結果件数表示
+    document.getElementById("resultCount").replaceChildren(resultCount);
+}
+
+// URLパラメータ取得
+const url = new URL(window.location.href);
+const params = url.searchParams;
+
+// indexのパラメータ取得
+const mode = params.get("mode");
+
+// 初期化
+let memoriaJsonCopy = null;
+if (mode == "lowrare") {
+    memoriaJsonCopy = JSON.parse(JSON.stringify(lrmemoriaJson));
+    document.getElementById("low").classList.remove("active");
+    document.getElementById("high").classList.add("active");
+} else {
+    memoriaJsonCopy = JSON.parse(JSON.stringify(memoriaJson));
+}
+for (let id in memoriaJsonCopy) {
+    memoriaJsonCopy[id]["tr"] = "";
+}
+makeTable();
+let charaSelectionArray = [];
+let typeSelectionArray = [];
+let onlymode = false
